2b22d0f53b9fd4d36c4844300b60bec48c6acc6f
Francisco <fcoajunior@gmail.com>
2019-04-12 12:51:52 -0300

    Adiciona o consultar por codigo

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -2,24 +2,36 @@ const qr = require('qr-image');
 const fs = require('fs');
 
 module.exports = function(app){
+    
+
+    //lista inventário
     app.get('/machine', function(req, res){
         var connection = app.persistence.connectionFactory;
-        //já que o DAO tem um prototype então tenho que ter o new para garantir que cada thread tem um dao
         var machineDAO = new app.persistence.machineDAO(connection);
+
         machineDAO.list(function(error, queryResult){
             //var string=JSON.stringify(queryResult);
             //json =  JSON.parse(string);
             res.send(queryResult);
         });
-        
+    });
+    
+    //consulta por codigo
+    app.get('/machine/consult/:code', function(req, res){
+        var code = req.params.code;
+        var connection = app.persistence.connectionFactory;
+        var machineDAO = new app.persistence.machineDAO(connection);
 
+        machineDAO.findByCode(code, function(error, queryResult){
+            var machine = queryResult[0];
+            res.send(machine);
+        });
     });
 
+    //Cadastra nova máquina
     app.post('/machine', function(req, res){
         var machine = req.body;
-
         var connection = app.persistence.connectionFactory;
-        
         var machineDAO = new app.persistence.machineDAO(connection);
         
         machineDAO.insert(machine, function(error, queryResult){
@@ -44,7 +56,6 @@ module.exports = function(app){
                         if(err){
                             console.log(err);
                         }
-                        
                     })
 
                     // Send the link of generated QR code
@@ -54,5 +65,7 @@ module.exports = function(app){
                 });
             }
         });
+
+        
     })
 }
