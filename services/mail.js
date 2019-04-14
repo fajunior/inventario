f11ddf682a32a8b04b054541f042adbad42ae2d8
Francisco <fcoajunior@gmail.com>
2019-04-14 16:30:12 -0300

    Ajust no teste

--- services/mail.js
+++ services/mail.js
@@ -1,32 +1,24 @@
+const fs = require('fs');
 function mail() {
 
 }
 
 mail.prototype.sendMail = function (qrcodeName, callback) {
-    // Require'ing module and setting default options
+    var configFile = fs.readFileSync('config/config.json');
+    var config = JSON.parse(configFile);
 
     var send = require('gmail-send')({
-        //var send = require('../index.js')({
-        user: 'fajunior@gmail.com',
-        // user: credentials.user,                  // Your GMail account used to send emails
-        pass: 'aykglcecybjfwwjf',
-        // pass: credentials.pass,                  // Application-specific password
-        to: 'fcoajunior@gmail.com',
-        // to:   credentials.user,                  // Send to yourself
-        // you also may set array of recipients:
-        // [ 'user1@gmail.com', 'user2@gmail.com' ]
-        // from:    credentials.user,            // from: by default equals to user
-        // replyTo: credentials.user,            // replyTo: by default undefined
-        // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
-        subject: 'test subject',
-        text: 'gmail-send example 1', // Plain text
-        //html:    '<b>html text</b>'            // HTML
-    });
-
-    var filepath = 'public/qrcode/' + qrcodeName; // File to attach
-
-    send({ // Overriding default parameters
+        user: config.username,
+        pass: config.password,
+        to: config.destinationMail,
         subject: 'attached ' + filepath, // Override value set as default
+        html: '<p>Equipamento cadastrado com sucesso</p>'
+    });
+    
+    var filepath = 'public/qrcode/' + qrcodeName; // File to attach
+    
+    send({ // Overriding default parameters
+        subject: config.subjectMail,
         files: [filepath],
     }, function (err, res) {
         console.log('* [example 1.1] send() callback returned: err:', err, '; res:', res);
