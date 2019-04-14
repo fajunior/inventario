34a84d95ad8ad45105571d17e6fe7b16114fda1a
Francisco <fcoajunior@gmail.com>
2019-04-14 17:01:46 -0300

    Ajuste no código de retorno http

--- public/js/remove.js
+++ public/js/remove.js
@@ -6,11 +6,12 @@ function removeConfirm(element) {
 }
 
 function removeMachine(element) {
+    console.log(element);
     var linha = element.parentNode.parentNode;
     var code = linha.getElementsByTagName('td')[0].textContent;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 202) {
             var response = this.responseText;
             if (response == 'removido') {
                 linha.classList.add('fadeOut');
--- tests/machineDAO.test.js
+++ tests/machineDAO.test.js
@@ -1,54 +1,13 @@
 //const test = require('tape');
-var tape = require('tape')
-var _test = require('tape-promise').default // <---- notice 'default'
-var test = _test(tape) // decorate tape
+var test = require('tape')
 
-
-var connection = require('../persistence/connectionFactory')();
-var dao = require('../persistence/machineDAO');
-
-var machineDAO = new dao(connection);
-
-var buscarCodigo = function (dao1) {
-    return new Promise((resolve, reject) => {
-        console.log('passo1');
-        dao1.getCode(function (erro, queryResult) {
-            console.log('entrou');
-            var code = queryResult[0];
-            resolve(code);
-        });
-    });
-}
-
-test('Connection não nula', (t) => {
-
-    t.assert(connection != null, "Descontou corretamente")
-    t.end()
-})
-
-test('DAO não nulo', (t) => {
-
-    t.assert(machineDAO != null, "Descontou corretamente")
-    t.end()
-})
-/*
-    buscarCodigo().then((resultado) => {
-        console.log(resultado);
-        
-        test('Maior codigo não zero', (t) => {
-            t.assert(1 == 1, "Descontou corretamente");
-            t.end();
-        });
-    });
-*/
 test('Maior codigo não zero', (t) => {
-    return buscarCodigo(machineDAO).then((resultado) => {
-        console.log(resultado);
-        
-        t.assert(1 == 1, "Descontou corretamente");
-        t.end();
+    var connection = require('../persistence/connectionFactory')();
+    var dao = require('../persistence/machineDAO')();
+    var machineDAO = new dao(connection);
+    machineDAO.getCode(function (error, queryResult) {
+        console.log(queryResult.codigo);
     });
-
-})
-/*
-*/
+    t.assert(1 == 1, "Descontou corretamente");
+    t.end();
+});
